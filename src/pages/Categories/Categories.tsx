66072004a0d82c0a3d1a8963fe7010fbee467def
Viola <violetta.artiukhova@skey.team>
2024-08-24 12:27:56 +0200

    implement mobile styles for 3 components

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -3,8 +3,8 @@ import CategoriesItem from "../CategoriesItem/CategoriesItem";
 import "./Categories.css";
 const Categories = () => {
   return (
-    <div id="categories">
-      <h1 className="productsBose">
+    <div id="categories" className="productsCategories">
+      <h1 className="productsTitle">
         Browse Bose
         <br /> products by category
       </h1>
